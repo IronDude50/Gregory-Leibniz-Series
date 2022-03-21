bf2c62588fa74a5a5ff343dc358ddab3ab08b9d4
IronDude50 <101145918+IronDude50@users.noreply.github.com>
2022-03-21 21:02:41 +1100

    Add files via upload

---
+++ Gregory-Leibniz Series/Gregory-Leibniz Series/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtIterations = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 30)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Iterations"
+        '
+        'txtIterations
+        '
+        Me.txtIterations.Location = New System.Drawing.Point(122, 12)
+        Me.txtIterations.Name = "txtIterations"
+        Me.txtIterations.Size = New System.Drawing.Size(100, 23)
+        Me.txtIterations.TabIndex = 1
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Enabled = False
+        Me.txtOutput.Location = New System.Drawing.Point(12, 42)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(210, 23)
+        Me.txtOutput.TabIndex = 2
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(12, 71)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 3
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(147, 71)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(230, 102)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.txtIterations)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Gregory-Leibniz Serie"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtIterations As TextBox
+    Friend WithEvents txtOutput As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Gregory-Leibniz Series/Gregory-Leibniz Series/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+        ' Closes the program
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        If txtIterations.Text <> "" Then
+            If IsNumeric(txtIterations.Text) = True Then
+                Dim Iterations As Integer = txtIterations.Text
+                Dim Output As Double
+                ' Declaration of variables
+
+                For k = 1 To Iterations
+                    Output += 4 * (((-1) ^ (k + 1)) / ((2 * k) - 1))
+                Next
+                ' Loops through each iteration
+
+                txtOutput.Text = Output
+                ' Displays output
+            Else
+                MessageBox.Show("Input is invalid", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                txtIterations.Clear()
+                ' When the input non-existent or in the wrong format
+            End If
+        Else
+            MessageBox.Show("Input is invalid", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            txtIterations.Clear()
+        End If
+        ' Validation
+    End Sub
+End Class
